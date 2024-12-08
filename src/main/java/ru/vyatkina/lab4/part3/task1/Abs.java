bda6dfa43602d2101f567bf5dc0a5bb0d327b935
ssixvfivess <ssixvfivess@yandex.ru>
2024-12-08 20:09:47 +0500

    4Lab

--- src/main/java/ru/vyatkina/lab4/part3/task1/Abs.java
+++
@@ -1,8 +0,0 @@
-package ru.vyatkina.lab4.part3.task1;
-
-public class Abs implements Applyable<Double, Double>{
-    @Override
-    public Double apply(Double aDouble) {
-        return Math.abs(aDouble);
-    }
-}
--- src/main/java/ru/vyatkina/lab4/part3/task1/StringToLength.java
+++
@@ -1,8 +0,0 @@
-package ru.vyatkina.lab4.part3.task1;
-
-public class StringToLength implements Applyable<String, Integer>{
-    @Override
-    public Integer apply(String s) {
-        return s.length();
-    }
-}
